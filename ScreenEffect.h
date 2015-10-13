f37ddfcb7c98a576f62ed61131213ea62e70f12a
Andrew King <pepsiotaku@gmail.com>
2015-10-12 20:39:49 -0400

    0.30.13
    Fixed some documentation in ScreenEffect.h

--- ScreenEffect.h
+++ ScreenEffect.h
@@ -53,11 +53,18 @@ namespace RPG {
 				changed during "Tint Screen" event commands.
 			*/
 			int transitionTimer;
-			int flashR;
-			int flashG;
-			int flashB;
+			int flashR; //!< Red value during "Flash Screen"
+			int flashG; //!< Green value during "Flash Screen"
+			int flashB; //!< Blue value during "Flash Screen"
 				int _unknown_48;
 				int _unknown_4C;
+			
+			/*! \brief "Flash Screen" timer (in frames)
+				
+				Start with the # of frames to transition from, and each frame will 
+				count backwards. Keep in mind 1.0 Secs = 60 frames. This value is
+				changed during "Flash Screen" event commands.
+			*/
 			int flashTimer;
 			
 			/*! \brief Shake strength
@@ -68,6 +75,13 @@ namespace RPG {
 			int shakeSpd; //!< Shake speed value used in "Shake Screen" command (0-9). Can be set higher than the default
 			int shakeOffsetX; //!< The screen offset value during a "Shake Screen" command.
 				int _unknown_60; // shakeOffsetY somehow?
+			
+			/*! \brief "Shake Screen" timer (in frames)
+				
+				Start with the # of frames to transition from, and each frame will 
+				count backwards. Keep in mind 1.0 Secs = 60 frames. This value is
+				changed during "Shake Screen" event commands.
+			*/
 			int shakeTimer;
 			
 			
