a6aaa9312981850814f6ea9e0a9753fbd72e7d7d
Matthew Fioravante <fmatthew5876@gmail.com>
2020-04-16 00:19:00 -0400

    Add BattleEvent fields and add BattleEvent2k

--- BattleEvent.h
+++ BattleEvent.h
@@ -2,6 +2,31 @@ namespace RPG {
 	//! Stub
 	typedef void BattleEventScriptData;
 	
+	/*! \brief Used to access battle event parameters in RM2000 (NOT the scriptlines).
+	*/
+	class BattleEvent2k {
+		public:
+			void **vTable;
+			BattleEventScriptData *eventData; //!< Stub. Use RPG::getBattleEventLine(monsterGroup,battleEventPage,lineId) to access battle event data
+			char _unknown_08;
+			char _unknown_09;
+			char _unknown_0A;
+			bool key_input_wait;
+			int key_input_variable;
+			bool key_input_all_direction;
+			bool key_input_decision;
+			bool key_input_cancel;
+			bool key_input_shift_2k;
+			bool key_input_down_2k; // In 2k3, this is wait frame for wait command
+			bool key_input_left_2k;
+			bool key_input_right_2k;
+			bool key_input_up_2k;
+			int wait_command_wait;
+			int _unknown_1C;
+			bool eventPageDone[100]; //!< Determines whether a particular battle event page has finished executing
+			
+	};
+
 	/*! \brief Used to access battle event parameters (NOT the scriptlines).
 
 		\sa RPG::battleEvents
@@ -10,27 +35,28 @@ namespace RPG {
 		public:
 			void **vTable;
 			BattleEventScriptData *eventData; //!< Stub. Use RPG::getBattleEventLine(monsterGroup,battleEventPage,lineId) to access battle event data
-				char _unknown_08;
-				char _unknown_09;
-				char _unknown_0A;
-				char _unknown_0B;
-				int _unknown_0C;
-				char _unknown_10;
-				char _unknown_11;
-				char _unknown_12;
-				int _unknown_14;
-				char _unknown_18;
-				char _unknown_19;
-				int _unknown_1C;
-				int _unknown_20;
-				char _unknown_24;
-				char _unknown_25;
-				char _unknown_26;
-				char _unknown_27;
-				char _unknown_28;
-				char _unknown_29;
-				char _unknown_2A;
-				char _unknown_2B; // unused?
+			char _unknown_08;
+			char _unknown_09;
+			char _unknown_0A;
+			bool key_input_wait;
+			int key_input_variable;
+			bool key_input_all_direction;
+			bool key_input_decision;
+			bool key_input_cancel;
+			bool _unknown_13;
+			int wait_command_wait;
+			bool command_wait_wait_for_key;
+			bool key_input_timed;
+			int key_input_time_variable;
+			int key_input_timed_frames;
+			bool key_input_numbers;
+			bool key_input_operators;
+			bool key_input_shift_2k3;
+			bool key_input_down_2k3;
+			bool key_input_left_2k3;
+			bool key_input_right_2k3;
+			bool key_input_up_2k3;
+			char _unknown_2B; // unused?
 			bool eventPageDone[100]; //!< Determines whether a particular battle event page has finished executing
 			
 	};
