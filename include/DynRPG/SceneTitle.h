46c7880c4d33e4a04c3cd130ab8c1ddf107e3ddc
Andrew King <pepsiotaku@gmail.com>
2021-05-18 16:05:41 -0400

    isLoadNotDefault > notComingFromLoadMenu, showingLogo

--- include/DynRPG/SceneTitle.h
+++ include/DynRPG/SceneTitle.h
@@ -2,7 +2,7 @@ namespace RPG {
 	//! Not implemented yet
 	typedef void AuroraBoard;
 
-	/*! \brief Access to the title screen parameters. 
+	/*! \brief Access to the title screen parameters.
 		\sa RPG::title
 	*/
 	class SceneTitle {
@@ -11,7 +11,7 @@ namespace RPG {
 				AuroraBoard *AuroraBoard1; //!< Not even a little bit implemented...
 				AuroraBoard *AuroraBoard2; //!< Not even a little bit implemented...
 			bool initialized; //!< Has the title screen initialized?
-			bool isLoadNotDefault; //!< Set to false to make Load the default option;
+			bool notComingFromLoadMenu; //!< true if not coming from the Load menu. Set to false to make Load the default option;
 			bool saveFilesPresent; //!< Are save files present (controls whether load can be selected). Determined DURING drawing of the title screen.
 				bool _unknown_0F;
 			Image *backgroundImage; //!< The title screen's background image. Can only be called after it's been drawn.
@@ -20,11 +20,11 @@ namespace RPG {
 	};
 
 	/*! \ingroup game_objects
-		\brief Access to the title screen parameters. 
-		
+		\brief Access to the title screen parameters.
+
 		\warning Most of the parameters can't be used in onInitTitleScreen, because they haven't
 			been created yet. Use onFrame instead to catch them when they have.
-		
+
 		Example:
 		\code
 if (RPG::title->winStartLoadEnd) { // Moves the title screen window to position 0,32.
@@ -34,4 +34,6 @@ if (RPG::title->winStartLoadEnd) { // Moves the title screen window to position 
 		\endcode
 	*/
 	static RPG::SceneTitle *&title = (**reinterpret_cast<RPG::SceneTitle ***>(0x4CDB94));
+
+	static bool &showingLogo = (**reinterpret_cast<bool **>(0x4CDADC));
 }
