27b0fb5b861d4de9eb7d0c4458d976994a349334
Andrew King <pepsiotaku@gmail.com>
2024-04-23 17:06:54 -0400

    set DYNRPG_SHARED=OFF

--- build_mingw.bat
+++ build_mingw.bat
@@ -9,8 +9,8 @@ cd build
 
 @REM echo %Path%
 
-@REM cmake.exe -G "MinGW Makefiles" -DDYNRPG_SHARED=ON -DCMAKE_BUILD_TYPE=Release -DDYNRPG_BUILD_DOCS=OFF ..
-cmake.exe -G "MinGW Makefiles" -DDYNRPG_SHARED=ON -DCMAKE_BUILD_TYPE=Release ..
+@REM cmake.exe -G "MinGW Makefiles" -DDYNRPG_SHARED=OFF -DCMAKE_BUILD_TYPE=Release -DDYNRPG_BUILD_DOCS=OFF ..
+cmake.exe -G "MinGW Makefiles" -DDYNRPG_SHARED=OFF -DCMAKE_BUILD_TYPE=Release ..
 cmake.exe --version
 mingw32-make.exe -j8
 
